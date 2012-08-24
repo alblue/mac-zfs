93b251f2d84da1cc258efcaea872076b05c084ec
BjoKaSH <googlelogin@bjoern-kahl.de>
2012-08-24 18:47:39 +0200

    Added post-install script to fix permissions on zfs.kext.
    Also added command to release script to  fixup permissions before
    packaging.

--- support/release.sh
+++ support/release.sh
@@ -25,15 +25,18 @@ mkdir -p ${DIR}/../${BUILD}
 rm -rf ${DIR}/../${BUILD}/ZFS105
 rm -rf ${DIR}/../${BUILD}/ZFS106
 cp -R ${DIR}/MacZFS.pmdoc ${DIR}/../${BUILD}
+cp -R ${DIR}/post-install.sh ${DIR}/../${BUILD}
 sed -i~  -e "s/0\\.0\\.0/${VERSION}/g" ${DIR}/../${BUILD}/MacZFS.pmdoc/*
 
 cd ${DIR}/..
 xcodebuild -sdk macosx10.5 -configuration ${CONFIG} -parallelizeTargets install INSTALL_OWNER=${USER} INSTALL_GROUP=${GROUP} SYMROOT=${BUILD}/${CONFIG}105 DSTROOT=${BUILD}/ZFS105 || exit 2
 
+chmod -Rv u+w,go-w,a+r ${BUILD}/ZFS105
 # No point in building on 10.5, it'll just do the same thing again
 if [ "`sysctl -b kern.osrelease`" != "9.8.0" ] 
 then
 	xcodebuild -sdk macosx10.6 -configuration ${CONFIG} -parallelizeTargets install INSTALL_OWNER=${USER} INSTALL_GROUP=${GROUP} SYMROOT=${BUILD}/${CONFIG}106 DSTROOT=${BUILD}/ZFS106 || exit 3
+	chmod -R u+w,go-w,a+r ${BUILD}/ZFS106
 fi
 
 cd ${BUILD}
